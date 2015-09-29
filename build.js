2510bff1a78fe9418806c7e9aedd10c8a5cdf3e5
Marc Laval <mlaval@amadeus.com>
2015-09-29 17:35:35 +0200

    Second

--- build.js
+++ build.js
@@ -1,3 +1,4 @@
 console.log('process.env.TRAVIS_PULL_REQUEST=' + process.env.TRAVIS_PULL_REQUEST);
 console.log('process.env.TRAVIS_BRANCH=' + process.env.TRAVIS_BRANCH);
-console.log('process.env.MY_VAR' + process.env.MY_VAR);
+console.log('process.env.MY_VAR=' + process.env.MY_VAR);
+console.log('process.env.DYN_VAR=' + process.env.DYN_VAR);
